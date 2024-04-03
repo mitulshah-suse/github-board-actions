af265bf5bafa88b878b72cddc55864d33b6fbc82
mitulshah-suse <mitul.shah@suse.com>
2024-04-03 17:48:56 +0530

    Add rancherlabs/image-scanning repo issues support

--- addIssues2InfracloudProject.sh
+++ addIssues2InfracloudProject.sh
@@ -1,15 +1,24 @@
-
-
 owner_name="rancher"
 repo_list="cis-operator rancher qa-tasks"
 label_name="team/infracloud"
 project_number=67
 
+#Fetch project ID using project number
+
+project_id=`gh api graphql -f query="
+query{
+    repository(owner: \"${owner_name}\", name: "rancher") {
+        projectV2(number: ${project_number}) {
+            id
+        }
+    }
+}" --jq '.data.repository.projectV2.id'`
+
+echo $project_id
+
 for repo_name in $repo_list
 do
-
     #Fetch issues from the repo which are currently not being tracked in the project
-
     issues=`gh api graphql -f query="
     query{
         repository(owner: \"${owner_name}\", name: \"${repo_name}\"){
@@ -30,23 +39,9 @@ do
 
     echo $issues
 
-    #Fetch project ID using project number
-
-    project_id=`gh api graphql -f query="
-    query{
-        repository(owner: \"${owner_name}\", name: \"${repo_name}\") {
-            projectV2(number: ${project_number}) {
-                id
-            }
-        }
-    }" --jq '.data.repository.projectV2.id'`
-
-    echo $project_id
-
     for issue_id in $issues
     do
-        echo Adding $issue_id to $project_id
-
+        echo "Adding $issue_id to $project_id"
         id=`gh api graphql -f query="
         mutation {
             addProjectV2ItemById(input: {contentId: \"${issue_id}\", projectId: \"${project_id}\"}) {
@@ -56,7 +51,41 @@ do
             }
         }"`
         echo $id
-
     done
+done
 
+#Fetch rancherlabs/image-scanning repo issues with team/infracloud label
+issues=`gh api graphql -f query="
+query{
+    repository(owner: "rancherlabs", name: "image-scanning"){
+        issues(first: 100, states: OPEN, labels: \"${label_name}\") {
+            nodes{
+                number
+                id
+                projectsV2(first: 100){
+                    nodes{
+                        id
+                        number
+                    }
+                }
+            }
+        }
+    }
+}" --jq ".data.repository.issues.nodes | map(select((.projectsV2.nodes == [] or all(.projectsV2.nodes[]; .number != ${project_number})))) | .[].id"`
+
+echo $issues
+
+#Adding rancherlabs/imagescanning issues to Infracloud project board
+for issue_id in $issues
+do
+    echo "Adding $issue_id to $project_id"
+    id=`gh api graphql -f query="
+    mutation {
+        addProjectV2ItemById(input: {contentId: \"${issue_id}\", projectId: \"${project_id}\"}) {
+            item { 
+                id
+            }
+        }
+    }"`
+    echo $id
 done
